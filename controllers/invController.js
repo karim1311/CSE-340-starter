ee6d91f4af3f4e0b9a5b1f4202d4adfe8dc6b7bb
Karimasapliaciones <karim.valenzuela@asaplicacion.com>
2026-03-19 13:58:09 -0700

    progr w3

---
+++ controllers/invController.js
@@ -0,0 +1,24 @@
+const invModel = require("../models/inventory-model")
+const utilities = require("../utilities")
+
+const invCont = {}
+
+/* ****************************
+ *  Build inventory by classification view
+ * ******************************* */
+invCont.buildByClassificationId = async function (req, res, next) {
+    const classification_id = req.params.classification_id
+    console.log('class id '+ classification_id)
+    const data = await invModel.getInventoryByClassificationId(classification_id)
+    console.log(data)
+    const grid = await utilities.buildClassificationGrid(data)
+    let nav = await utilities.getNav()
+    const className = data[0].classification_name
+    res.render("./inventory/classification", {
+        title: className + " vehicles",
+        nav,
+        grid,
+    })
+}
+
+module.exports = invCont
--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController")
 
 
 /* ***********************
@@ -27,9 +29,8 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
-app.get("/", function(req, res) {
-  res.render("index", {title:"Home"})
-})
+app.get("/", baseController.buildHome)
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
